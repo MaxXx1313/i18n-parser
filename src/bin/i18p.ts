95fb5198ce2b02a65485fbdd1bade22c932c29da
Maksim Chertkov <maxch@unl.solutions>
2023-08-15 15:45:00 +0200

    rc config

--- src/bin/i18p.ts
+++ src/bin/i18p.ts
@@ -5,14 +5,19 @@ import * as process from "process";
 import * as assert from "assert";
 import * as yargs from "yargs";
 import { ArgumentsCamelCase } from "yargs";
-// import {findUpSync} from 'find-up';
+import * as findUp from 'find-up';
+import * as fs from "fs";
 
 
-// const configPath = findUpSync(['.i18prc']);
-// const config = configPath ? JSON.parse(fs.readFileSync(configPath, {encoding: "utf-8"}).toString()) : {};
+const configPath = findUp.sync(['.i18prc', 'i18p.json']);
+if (configPath) {
+    console.debug('Using config:', configPath);
+}
+
+const config = configPath ? JSON.parse(fs.readFileSync(configPath, {encoding: "utf-8"}).toString()) : {};
 
 yargs(hideBin(process.argv))
-    // .config(config)
+    .config(config)
     .command('parse', 'parse google spreadsheet and create i18n files', () => {
     }, async (argv: ArgumentsCamelCase<any>) => {
         assert.ok(argv.id, '--id is required');
