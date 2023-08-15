dcad48c83aac8f8428508c80600f2a7624cfa090
Maksim Chertkov <maxch@unl.solutions>
2023-08-15 14:05:02 +0200

    code style

--- src/googlesheet/googlesheet.ts
+++ src/googlesheet/googlesheet.ts
@@ -1,27 +1,7 @@
-import { GoogleSheetsClient } from "./client";
+import { GoogleSheetsClient } from "./google-sheets-client";
 import { JsonToken, LangData } from "../parser";
 
 const serviceAccountKeyFile = "./config/i18n-sync@b-synch.iam.gserviceaccount.com.json";
-
-/**
- * @more https://developers.google.com/sheets/api/guides/concepts
- */
-export async function parseSpreadsheet(spreadsheetId: string) {
-    // Generating google sheet client
-    const googleSheetClient = new GoogleSheetsClient(serviceAccountKeyFile);
-
-    // Reading Google Sheet from a specific range
-    const data = await googleSheetClient.readGoogleSheet(spreadsheetId, "1:1");
-    console.log(data);
-    await googleSheetClient.writeGoogleSheet(spreadsheetId, "A2", [['test']]);
-
-    // // Adding a new row to Google Sheet
-    // const dataToBeInserted = [
-    //     ['11', 'rohith', 'Rohith', 'Sharma', 'Active'],
-    //     ['12', 'virat', 'Virat', 'Kohli', 'Active']
-    // ]
-    // await _writeGoogleSheet(googleSheetClient, sheetId, tabName, range, dataToBeInserted);
-}
 
 
 /**
@@ -62,7 +42,22 @@ export async function publishSpreadsheet(spreadsheetId: string, data: LangData) 
 }
 
 
-// const data = await googleSheetClient.readGoogleSheet(spreadsheetId, "1:1");
-// console.log(data);
-// await googleSheetClient.writeGoogleSheet(spreadsheetId, "A1", rows);
+/**
+ * @more https://developers.google.com/sheets/api/guides/concepts
+ */
+export async function parseSpreadsheet(spreadsheetId: string) {
+    // Generating google sheet client
+    const googleSheetClient = new GoogleSheetsClient(serviceAccountKeyFile);
 
+    // Reading Google Sheet from a specific range
+    const data = await googleSheetClient.readGoogleSheet(spreadsheetId, "1:1");
+    console.log(data);
+    await googleSheetClient.writeGoogleSheet(spreadsheetId, "A2", [['test']]);
+
+    // // Adding a new row to Google Sheet
+    // const dataToBeInserted = [
+    //     ['11', 'rohith', 'Rohith', 'Sharma', 'Active'],
+    //     ['12', 'virat', 'Virat', 'Kohli', 'Active']
+    // ]
+    // await _writeGoogleSheet(googleSheetClient, sheetId, tabName, range, dataToBeInserted);
+}
