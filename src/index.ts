3a86a2c711e7128d035115f6919fa12b9d451feb
Maksim Chertkov <maxch@unl.solutions>
2023-08-25 17:27:28 +0200

    parse json

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+import { jsonParseFile } from "./parser/json_parse";
+
 console.log('hello');
+
+
+const result = jsonParseFile('./assets/sample1/en.json');
+
+console.log(result);
---
+++ src/parser/json_parse.ts
@@ -0,0 +1,46 @@
+import * as fs from "fs";
+import { anyobject, isObject, plainObject } from "./xpath";
+
+/**
+ * Parse json file and produce structured result
+ */
+export function jsonParseFile(path: string) {
+    const jsonDataStr = fs.readFileSync(path, {encoding: "utf-8"});
+    let data = JSON.parse(jsonDataStr);
+    return jsonParse(data);
+}
+
+/**
+ * Parse json structure
+ */
+export function jsonParse(jsonData: anyobject) {
+
+    const result: JsonToken[] = [];
+
+    for (const key in jsonData) {
+        const value = jsonData[key];
+
+        if (isObject(value)) {
+
+            const plainObj = plainObject(value, key);
+            for (const plainObjKey in plainObj) {
+                result.push({
+                    key: plainObjKey,
+                    value: plainObj[plainObjKey],
+                });
+            }
+        } else {
+            result.push({
+                key,
+                value,
+            });
+        }
+    }
+    return result;
+}
+
+
+export interface JsonToken {
+    key: string;
+    value: string;
+}
